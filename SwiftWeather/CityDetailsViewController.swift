0a5fd633ae170f84aad9609c76325e88035cd599
Aleksandar Mihailovski <aleksandar.mihailovski@navayo.com>
2016-04-01 22:29:26 +0200

    Add initial layout
    City list screen
    City details screen
    Add city screen

---
+++ SwiftWeather/CityDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CityDetailsViewController.swift
+//  SwiftWeather
+//
+//  Created by Aleksandar Mihailovski on 4/1/16.
+//  Copyright © 2016 Aleksandar Mihailovski. All rights reserved.
+//
+
+import UIKit
+
+class CityDetailsViewController: UIViewController {
+    
+}
---
+++ SwiftWeather/CityListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CityListViewController.swift
+//  SwiftWeather
+//
+//  Created by Aleksandar Mihailovski on 4/1/16.
+//  Copyright © 2016 Aleksandar Mihailovski. All rights reserved.
+//
+
+import UIKit
+
+class CityListViewController: UITableViewController {
+    
+}
