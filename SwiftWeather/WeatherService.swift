0e2cbf84ce211bf41696bbf553258f4859defb0e
Aleksandar Mihailovski <aleksandar.mihailovski@navayo.com>
2016-04-02 05:32:45 +0200

    Rrsolving issue with http connection

---
+++ SwiftWeather/WeatherService.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherService.swift
+//  SwiftWeather
+//
+//  Created by Aleksandar Mihailovski on 4/2/16.
+//  Copyright © 2016 Aleksandar Mihailovski. All rights reserved.
+//
+
+import Foundation
